45717cf9cd4237f9e0ed7e49568a7926bf4e0189
MatsBACCANO <43158600+Takashi-Matsumura@users.noreply.github.com>
2023-10-26 01:04:47 +0000

    add card sample components

--- app/helpers/page.tsx
+++ app/helpers/page.tsx
@@ -1,7 +1,74 @@
 import React from "react";
+import { Metadata } from "next";
+import Image from "next/image";
 
-const page = () => {
-  return <div>page</div>;
+import { cn } from "@/lib/utils";
+
+// import { DemoCookieSettings } from "./components/cookie-settings";
+// import { DemoCreateAccount } from "./components/create-account";
+// import { DemoDatePicker } from "./components/date-picker";
+// import { DemoGithub } from "./components/github-card";
+import { DemoNotifications } from "./components/notifications";
+// import { DemoPaymentMethod } from "./components/payment-method";
+// import { DemoReportAnIssue } from "./components/report-an-issue";
+import { DemoShareDocument } from "./components/share-document";
+import { DemoTeamMembers } from "./components/team-members";
+
+export const metadata: Metadata = {
+  title: "Cards",
+  description: "Examples of cards built using the components.",
 };
 
-export default page;
+function DemoContainer({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div
+      className={cn(
+        "flex items-center justify-center [&>div]:w-full",
+        className
+      )}
+      {...props}
+    />
+  );
+}
+
+export default function CardsPage() {
+  return (
+    <>
+      <div className="md:hidden">
+        <Image
+          src="/examples/cards-light.png"
+          width={1280}
+          height={1214}
+          alt="Cards"
+          className="block dark:hidden"
+        />
+        <Image
+          src="/examples/cards-dark.png"
+          width={1280}
+          height={1214}
+          alt="Cards"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="items-start justify-center gap-6 rounded-lg p-8 ">
+        <div className="col-span-2 grid items-start gap-6 lg:col-span-1">
+          <DemoContainer>
+            <DemoTeamMembers />
+          </DemoContainer>
+          <DemoContainer>
+            <DemoShareDocument />
+          </DemoContainer>
+          {/* <DemoContainer>
+            <DemoDatePicker />
+          </DemoContainer> */}
+          <DemoContainer>
+            <DemoNotifications />
+          </DemoContainer>
+        </div>
+      </div>
+    </>
+  );
+}
